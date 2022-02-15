6e76eb7d30866c51ee6a124f6060c736792048f0
julianorchard <hello@julianorchard.co.uk>
2022-02-15 10:35:24 +0000

    adding detail to readme, and finishing marketing-images.bat

--- image-manipulation/marketing-images.bat
+++ image-manipulation/marketing-images.bat
@@ -12,24 +12,44 @@ for /r %%a in (oq\*) do (
 REM // ~nxa Gets File Name Only (the 'a' is %%a)
 	set p=%%a
 	set f=%%~nxa
+REM // ~dpa Gets The Directory Only
+	set d=%%~dpa
+
+REM // fout
+	set fout=  !f!
+
+REM // Update Output (original file name)
+	<NUL set /p ="Formatting !f!"
 
 REM // Remove Brackets
-		set f=!f:^(=!
-		set f=!f:^)=!
-		set f=!f: =-!
+	set f=!f:^(=!
+	set f=!f:^)=!
+	set f=!f: =-!
 
 REM // JPG File Fix Cases
-		set f=!f:^JPG=jpg!
-		set f=!f:^jpeg=jpg!
+	set f=!f:^JPG=jpg!
+	set f=!f:^jpeg=jpg!
 
 REM // Rename .jpg File
-		ren "!p!" "!f!"
+	ren "!p!" "!f!"
 
 REM // Use ImageMagick to Mirror into \lq Folder
-		magick convert -strip -interlace Plane -gaussian-blur 0.05 -quality 85% !p! ..\lq-!f!
+REM // settings copied from a helpful S/O answer
+	magick convert -strip -interlace Plane -gaussian-blur 0.05 -quality 85% "!d!\!f!" "!d!lq-!f!"
+	echo   !f! - converted to Low Quality
 
-REM // Echo
-		echo !f!
+REM // Messy, but works
+	cd !d!
+	cd ..\
+
+REM // Make \lq Dir, if it doesn't exist
+	if not exist lq\ mkdir lq
+
+REM // Copy temp lq file to the new place
+	copy /Y "!d!lq-!f!" "lq\!f!">NUL
+
+REM // Delete the temporary file
+	del "!d!lq-!f!"
 )
 
 endlocal
--- readme.markdown
+++ readme.markdown
@@ -1,4 +1,33 @@
-# Specialised Scripts
+# Scripts
 
-For use by those who created them. They are very one-purpose and only useful in
-certain situations. 
+These are singular purpose and intended to only be used by their creator.
+
+To see more general use scripts for all users, see [this
+repository](https://github.com/wessexlifts/all-users).
+
+
+## Marketing Images
+
+A batch script deployed in main image folder to rename images nicely (mainly remove the brackets and spaces from Windows default bulk renaming). Requires ImageMagick to resize the images and store them in the lq (low quality) folder. Folder structure is as follows;
+
+```
+	scripts
+	|   marketing-images.bat
+	|   
+	+---EG
+	|   +---final
+	|   |   +---lq
+	|   |   |       EG-1.jpg
+	|   |   |       
+	|   |   \---oq
+	|   |           EG-1.jpg
+	|   |           
+	|   \---raw
+	|           Unedited-EG (1).jpg
+	etc.
+```
+
+## More Information
+
+If you know what you're doing, feel free to make use of any part of these
+scripts. 
